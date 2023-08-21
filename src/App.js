2c7d41ee4def7d4bb95c69b35a30fdecf08144d2
Limon49 <mrlimon28@gmail.com>
2023-08-21 20:07:46 +0600

    update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
+
+function App() {
+  let myVariable = 345;
+  return (
+    <>
+    <h3>MY app</h3>
+    <p>my app work</p>
+    {/*<Header/>
+    <Todos/>
+    <Footer/>*/}
+    </>
+    );
+  }
+
+export default App;
